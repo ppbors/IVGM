0ef7bc126e2a18a0d45a33aa31f696a7017d8af5
L33t0r <l33t0r@users.noreply.github.com>
2019-12-04 14:10:00 +0100

    Disable shooting laser when game is paused

--- Assets/Scripts/CannonBehavior.cs
+++ Assets/Scripts/CannonBehavior.cs
@@ -4,11 +4,17 @@ using System.Collections;
 public class CannonBehavior : MonoBehaviour {
 
 	public GameObject m_laserPrefab;
+    public GameManagerScript gm;
 	
 	// Update is called once per frame
 	void Update() 
 	{
-		if (Input.GetKeyDown(KeyCode.Space))
+        // Do not shoot laser when game is paused
+        if(gm.IsPaused())
+        {
+            return;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
 		{
             // Instantiate laser
 			GameObject go = GameObject.Instantiate(m_laserPrefab, transform.position, transform.rotation) as GameObject;
