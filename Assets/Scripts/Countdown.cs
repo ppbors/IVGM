2d52136eddb3113483a246f48d3135b5dbaa3ffb
MartijnWester1 <martijn__wester@hotmail.com>
2019-12-22 17:46:10 +0100

    Asteroid hit sounds

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -52,7 +52,8 @@ public class Countdown : MonoBehaviour
     {
         while (true)
         {
-            this.GetComponent<AudioSource>().Play();
+            if (timeLeft>0)
+                this.GetComponent<AudioSource>().Play();
             if (timeLeft==0)
                 this.GetComponents<AudioSource>()[1].Play();
             yield return new WaitForSeconds(1);
---
+++ Assets/Scripts/LaserScripts/ShotBehavior1.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotBehavior1 : MonoBehaviour {
+
+    public ParticleSystem explosion;
+    private readonly float explosionSize = 3000.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        var main = explosion.main;
+        main.startSize = explosionSize;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // change to change speed of laser
+		transform.position += transform.forward * Time.deltaTime * 150;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        // shot destroys itself once it hits something with a trigger.
+        //if (!other.gameObject.name.Contains("Spawn"))
+            //GameObject.Destroy(this.gameObject);
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            float health = -1 * gameObject.transform.localScale.x * 10;
+            other.gameObject.GetComponent<PlayerController>().AddHealth(health);
+        }
+
+        if (other.gameObject.name.Contains("Asteroid"))
+        {
+            other.gameObject.GetComponent<AsteroidBehavior>().ChangeSize();
+            this.GetComponents<AudioSource>()[1].Play();
+        }
+        if (other.gameObject.name.Contains("Enemy") || other.gameObject.name.Contains("Boss"))
+            other.gameObject.GetComponent<EnemyControl>().DecreaseHealth();
+        explosion = GameObject.Instantiate(explosion, transform.position, transform.rotation) as ParticleSystem;
+        GameObject.Destroy(this.gameObject, 0.2f);
+    }
+
+}
